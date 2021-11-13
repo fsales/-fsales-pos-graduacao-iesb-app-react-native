98860fc80525d6e2e1f493a41f770311544120a3
Fábio Sales <fabio.oliveira.sales@gmail.com>
2021-11-13 20:35:15 -0300

    configurando stilos

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { style } from './styles';
+
 export default function SignIn() {
     return (
-        <View>
+        <View style={style.container}>
             <Text>Hello Word,</Text>
         </View>
     );
