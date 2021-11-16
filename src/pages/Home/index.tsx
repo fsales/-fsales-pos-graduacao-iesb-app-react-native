99c967389687a5bf4dcfe6732a052f3769497068
Fábio Sales <fabio.oliveira.sales@gmail.com>
2021-11-16 01:04:01 -0300

    header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,23 @@
+import { Feather } from '@expo/vector-icons';
 import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import Header from '../../components/Header';
+import { Container, Input, SearchButton, SearchContainer } from './styles';
 
 
 const Home = () => {
   return (
     <Container>
-      <Text>
-        tela Home
-      </Text>
+      <Header title="Sales Flix" />
+      <SearchContainer>
+        <Input
+          placeholder={'teste'}
+          placeholderTextColor="#ddd"
+        />
 
+        <SearchButton>
+          <Feather name='search' size={30} color='#FFF' />
+        </SearchButton>
+      </SearchContainer>
     </Container>
   )
 }
